93da524d07376d533da7ee96b9bac29b31ff1ad3
Allen <allenungureanu@gmail.com>
2018-11-19 19:55:17 +0100

    e-mail form 3

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+  if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailForm = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "allenungureanu@gmail.com";
+    $headers = "From: ".$mailForm;
+    $txt = "You have received an e-mail from ".$name. '.\n\n'.$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: e-mail.php?mailsend");
+  }
