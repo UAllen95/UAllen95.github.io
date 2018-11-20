49e60f4df53dbf674b17fd9b4be72610784d8e1f
Allen <allenungureanu@gmail.com>
2018-11-20 11:09:43 +0100

    form try

--- contactform.php
+++ contactform.php
@@ -3,13 +3,13 @@
   if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
-    $mailForm = $_POST['mail'];
+    $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
     $mailTo = "allenungureanu@gmail.com";
     $headers = "From: ".$mailForm;
-    $txt = "You have received an e-mail from ".$name. '.\n\n'.$message;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: e-mail.php?mailsend");
+    header("Location: e-mail.html?mailsend");
   }
