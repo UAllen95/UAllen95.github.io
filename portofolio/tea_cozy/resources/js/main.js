ce10caae9b96368ae0b8606001851b15ed2e8e3d
Allen <allenungureanu@gmail.com>
2019-04-02 20:58:24 +0200

    tea cozy - jquery & header

--- portofolio/tea_cozy/resources/js/main.js
+++ portofolio/tea_cozy/resources/js/main.js
@@ -1,17 +1,48 @@
 $(document).ready(function(){
-  $('.smooth-scroll').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-          || location.hostname == this.hostname) {
 
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-             if (target.length) {
-               $('html,body').animate({
-                   scrollTop: target.offset().top
-              }, 800);
-              return false;
-          }
-      }
+  // Navbar
+
+  $("#nav-toggle").on("click", function(event){
+    event.preventDefault();
+    $(".mobile-list").slideToggle();
+    $(this).toggleClass("active");
   });
+
+  $("header .mobile-list a").on("click", function(event){
+    event.preventDefault();
+    var headerHeight = $("header .navbar-fixed-top").height();
+    var id = $(this).attr("href");
+    var originalScrollCordinate = $(id).offset().top;
+    var newScrollCordinate = originalScrollCordinate - headerHeight;
+    $("html").animate({
+      scrollTop : newScrollCordinate
+    });
+  });
+
+  // Carousel
+
+//  var $galleryImg = $("#featured .item img");
+//  var $closeBtn = $(".btn");
+//  var $carousel = $(".carousel");
+//  var $overlay = $(".overlay");
+//
+//  $carousel.slick({
+//    centerMode: true,
+//    fade: true,
+//    arrows: false,
+//    dots: true,
+//    draggable: false
+//  });
+//
+//  $galleryImg.on("click", function(slick){
+//    var $imgId = $(this).attr("id");
+//    $overlay.fadeIn();
+//    $carousel.slick("slickGoTo", $imgId, true).slick("setPosition");
+//  });
+//
+//  $closeBtn.on("click", function(event){
+//    event.preventDefault();
+//    $overlay.fadeOut();
+//  });
 
 });
