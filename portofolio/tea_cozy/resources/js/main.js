629fba8126011267b006343fc9ebd9dfd83bc3bd
Allen <allenungureanu@gmail.com>
2019-09-08 00:27:00 +0200

    Final touch

--- portofolio/tea_cozy/resources/js/main.js
+++ portofolio/tea_cozy/resources/js/main.js
@@ -18,31 +18,18 @@ $(document).ready(function(){
       scrollTop : newScrollCordinate
     });
   });
-
-  // Carousel
-
-//  var $galleryImg = $("#featured .item img");
-//  var $closeBtn = $(".btn");
-//  var $carousel = $(".carousel");
-//  var $overlay = $(".overlay");
-//
-//  $carousel.slick({
-//    centerMode: true,
-//    fade: true,
-//    arrows: false,
-//    dots: true,
-//    draggable: false
-//  });
-//
-//  $galleryImg.on("click", function(slick){
-//    var $imgId = $(this).attr("id");
-//    $overlay.fadeIn();
-//    $carousel.slick("slickGoTo", $imgId, true).slick("setPosition");
-//  });
-//
-//  $closeBtn.on("click", function(event){
-//    event.preventDefault();
-//    $overlay.fadeOut();
-//  });
-
 });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
